642862812e3d391724ea35855a85b684092bee69
Gango Simeon <gangosimeon@gmail.com>
2024-10-01 15:55:49 +0000

    suppression d'un commentaire

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 // let zoneScore=document.querySelector(".zoneScore span");
-// console.log(zoneScore);
 // let zoneProposition=document.querySelector(".zoneProposition");
 // console.log(zoneProposition);
 // let optionSource=document.querySelectorAll(".optionSource input");
